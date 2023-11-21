e614b0361e6164b594a17f6aeb08713a520a20fa
felipe <130618161+fa9958189@users.noreply.github.com>
2023-11-20 21:01:34 -0300

    meu primeiro commit

---
+++ src/pages/logon/index.js
@@ -0,0 +1,19 @@
+import'./styles.css'
+
+
+export default function logon(){
+
+    return(
+        <div className="logon-container">
+            <section className="form">
+                <h1>faça seu login</h1>
+                <form>
+                    <input placeholder="Email" />
+                    <input placeholder="Senha" type='password'/>
+                    <button type="submit">Entrar</button>
+                    <a href="#">Novo Cadastro</a>
+                </form>
+            </section>
+        </div>
+    )
+}
