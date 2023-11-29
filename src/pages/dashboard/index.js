aba5f8e38be3bbfa7610b68ed773a784d660270e
felipe <130618161+fa9958189@users.noreply.github.com>
2023-11-28 21:31:17 -0300

    felipe

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -9,13 +9,15 @@ export default function Dashboard() {
     <div className="dashboard-container">
 
         <div className='menu'>
-            <h1>menu</h1>
+           
             <Menu />
         </div>
 
         <div className='principal'>
            <h1>pagina principal</h1>
-        </div>       
+        </div>
+       
+        
     </div>
   
     )
