c006192750253eb7f2afdef48d41f6123af3c773
Informática para Internet <fa9958189@gmail.com>
2024-03-18 21:36:13 -0300

    estoque feito

--- src/pages/listarEstoque/index.js
+++ src/pages/listarEstoque/index.js
@@ -1,38 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../pages/global.css';
 import Menu from '../../componente/Menu';
+import { FiTrash } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import Head from '../../componente/Head';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
-export default function Listaestoque() {
-  const entradas = JSON.parse(localStorage.getItem("cd-entradas") || "[]");
-  const saidas = JSON.parse(localStorage.getItem("cd-saidas") || "[]");
+export default function ListaEstoque() {
+  const [estoque, setEstoque] = useState([]);
+  const [error, setError] = useState(null);
 
-  const calcularEstoqueAtual = (id_produto) => {
-    const quantidadeEntrada = entradas.filter(entrada => entrada.id_produto === id_produto)
-                                       .reduce((total, entrada) => total + entrada.qtde, 0);
-    const quantidadeSaida = saidas.filter(saida => saida.id_produto === id_produto)
-                                  .reduce((total, saida) => total + saida.qtde, 0);
-    return quantidadeEntrada - quantidadeSaida;
+  useEffect(() => {
+    fetchEstoque();
+  }, []);
+
+  function fetchEstoque() {
+    fetch('http://localhost:5000/estoque')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao buscar estoque: ' + response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data && data.estoque) {
+          setEstoque(data.estoque);
+          setError(null);
+        } else {
+          setError('Erro ao carregar o estoque.');
+        }
+      })
+      .catch(error => {
+        console.error('Erro ao buscar estoque:', error);
+        setError('Erro ao carregar o estoque.');
+      });
+  }
+
+  const removerItem = (id) => {
+    fetch(`http://localhost:5000/estoque/${id}`, { method: 'DELETE' })
+      .then(response => response.json())
+      .then(data => {
+        alert(data.mensagem);
+        fetchEstoque();
+      })
+      .catch(error => console.error('Erro ao excluir item do estoque:', error));
   };
 
-  function mostrarnome(idproduto){
-    let nome= "";
-     const listarproduto = JSON.parse(localStorage.getItem("cd-produtos") || "[]");
-     listarproduto.
-                  filter(value => value.id ==idproduto).
-                  map(value => {
-                   
-                  nome=value.descricao;
-  
-  
-                      
-  
-  
-                })
-          return nome;
-          
-    }
+  const confirmarRemocao = (id) => {
+    confirmAlert({
+      title: 'Remover Item do Estoque',
+      message: 'Tem certeza de que deseja remover este item do estoque?',
+      buttons: [
+        {
+          label: 'Sim',
+          onClick: () => removerItem(id)
+        },
+        {
+          label: 'Não',
+          onClick: () => console.log('Ação de remoção cancelada')
+        }
+      ]
+    });
+  };
 
   return (
     <div className="dashboard-container">
@@ -40,22 +70,36 @@ export default function Listaestoque() {
         <Menu />
       </div>
       <div className='principal'>
-        <Head title="Lista Estoque" />
-  
+        <Head title="Listar Estoque" />
+
+        {error && <p>{error}</p>}
         <table>
           <thead>
             <tr>
-              <th>Id</th>
-              <th>Id do Produto</th>
-              <th>Quantidade</th>
+              <th>ID</th>
+              <th>Nome do Produto</th>
+              <th>Entradas</th>
+              <th>Saídas</th>
+              <th>Quantidade em Estoque</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {entradas.map((entrada) => (
-              <tr key={entrada.id}>
-                <td>{entrada.id}</td>
-                <td>{mostrarnome(entrada.id_produto)}</td>
-                <td>{calcularEstoqueAtual(entrada.id_produto)}</td>
+            {estoque.map((item) => (
+              <tr key={item.id_produto}>
+                <td>{item.id_produto}</td>
+                <td>{item.nome_produto}</td>
+                <td>{item.entradas}</td>
+                <td>{item.saidas}</td>
+                <td>{item.quantidade_em_estoque}</td>
+                <td className='botoes'>
+                  <FiTrash
+                    size={18}
+                    color='red'
+                    onClick={() => confirmarRemocao(item.id_produto)}
+                    cursor="pointer"
+                  />
+                </td>
               </tr>
             ))}
           </tbody>
