bf94bb4f5b735d70485788b4bcf37b22ea81f895
fa9958189 <fa9958189@gmail.com>
2024-06-26 21:43:15 -0300

    meu primeiro commit

--- src/pages/listaItensOrcamento/index.js
+++ src/pages/listaItensOrcamento/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../../pages/global.css';
 import { FiPlusCircle, FiPrinter, FiTrash } from "react-icons/fi";
-import { useParams,useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import Barrasuperior from '../../componente/Barrasuperior';
 import Head from '../../componente/Head';
 import carregar from '../../assets/img/loading.gif';
@@ -11,9 +11,6 @@ import api from '../../server/api';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
 
 export default function ListarItensOrcamento() {
   const { id } = useParams();
@@ -55,59 +52,56 @@ export default function ListarItensOrcamento() {
     }).format(valor);
   }
 
-  const load =()=>{
-    return(
+  const load = () => {
+    return (
       <>
         <img src={carregar} />
       </>
     )
   }
-  useEffect(()=>{
-   setTot(valor_unitario*qtde)
-  },[valor_unitario,qtde])
 
+  useEffect(() => {
+    setTot(valor_unitario * qtde)
+  }, [valor_unitario, qtde])
 
+  useEffect(() => {
+    aportavalor()
+  }, [id_produto])
 
-
- useEffect(()=>{
-aportavalor()
- },[id_produto])
   useEffect(() => {
     mostrardados();
     mostrarOrcamento();
     mostrarOrcamentos();
-
-    
-    
   }, [id]);
-  const abrirModal = () =>{
+
+  const abrirModal = () => {
     setShow(true);
   }
-  const abrirModalPagar = () =>{
+
+  const abrirModalPagar = () => {
     setShowPagar(true);
   }
-  async function aportavalor(){
-  if (id_produto == "undefined" || id_produto == null || id_produto == "") 
-  {
-    console.log ("id do produto nao foi definido")
-  }
-        else {
-        await api.get(`/entrada/valorunitario/${id_produto}`)
-          .then(resposta=>{
-            setValor_unitario(resposta.data.valorunitario)
-            setTot(valor_unitario*qtde)
-          })
-  .catch(error=>{
-        alert("Houve um erro ao consulta o valor unitário")
-  })
-  .finally(()=>{
-    alert("Processo finalizado!")
-  })
 
-}
-   }
- async function mostrardados() {
-  await  api.get('/produto')
+  async function aportavalor() {
+    if (id_produto == "undefined" || id_produto == null || id_produto == "") {
+      console.log("id do produto nao foi definido")
+    } else {
+      await api.get(`/entrada/valorunitario/${id_produto}`)
+        .then(resposta => {
+          setValor_unitario(resposta.data.valorunitario)
+          setTot(valor_unitario * qtde)
+        })
+        .catch(error => {
+          alert("Houve um erro ao consulta o valor unitário")
+        })
+        .finally(() => {
+          alert("Processo finalizado!")
+        })
+    }
+  }
+
+  async function mostrardados() {
+    await api.get('/produto')
       .then(res => {
         setProdutos(res.data.produtos);
       })
@@ -116,8 +110,8 @@ aportavalor()
       });
   }
 
-async  function mostrarOrcamento() {
-   await api.get(`/orcamento/${id}`)
+  async function mostrarOrcamento() {
+    await api.get(`/orcamento/${id}`)
       .then(response => {
         setOrcamento(response.data.orcamento);
         setNome(response.data.orcamento[0].nome);
@@ -136,13 +130,13 @@ async  function mostrarOrcamento() {
       console.error('Erro ao buscar orçamentos:', error);
     }
   }
-  
+
   const imprimirTabela = () => {
     window.print();
   };
 
-   const removerItem = async (id) => {
-   await api.delete(`/itensorcamento/${id}`)
+  const removerItem = async (id) => {
+    await api.delete(`/itensorcamento/${id}`)
       .then(res => {
         if (res.status === 200) {
           console.log(`Item com ID ${id} excluído com sucesso!`);
@@ -163,7 +157,7 @@ async  function mostrarOrcamento() {
       buttons: [
         {
           label: 'Sim',
-          onClick: () =>  removerItem(id)
+          onClick: () => removerItem(id)
         },
         {
           label: 'Não',
@@ -173,23 +167,27 @@ async  function mostrarOrcamento() {
     });
   };
 
-  const handleRecebidoChange = (e) => {
-    const valorRecebido = parseFloat(e.target.value);
+  useEffect(() => {
+
+  }, [recebido])
+
+  const handleRecebidoChange = (valor) => {
+    const valorRecebido = parseFloat(valor);
     setRecebido(valorRecebido);
     setTroco(valorRecebido - total);
   }
+
   const salvarDados = async () => {
     if (id_produto === "" || qtde === "" || valor_unitario === "") {
       alert("Há campos vazios");
     } else {
-    await  api.post('/itensorcamento', dados)
+      await api.post('/itensorcamento', dados)
         .then(res => {
           if (res.status === 201) {
             console.log(`Item adicionado com sucesso!`);
             mostrarOrcamento();
             mostrarOrcamentos(); // Atualiza a lista após a inserção
             fechaModal();
-          
           } else {
             console.error("Erro ao adicionar item", res.data);
           }
@@ -199,134 +197,173 @@ async  function mostrarOrcamento() {
         });
     }
   }
-  
+ const gerarParcelas = () =>{
+
+ }
   const salvarPagamento = async () => {
-    const pagamento ={
+    const pagamento = {
       recebido,
       troco,
       situacaoPagamento
     }
 
-      await api.post('/itensorcamento/pagamento', pagamento)
-        .then(res => {
-          if (res.status === 201) {
-            console.log(`Item adicionado com sucesso!`);
-            mostrarOrcamento();
-          } else {
-            console.error("Erro ao adicionar item", res.data);
-          }
-        })
-        .catch(error => {
-          console.error("Erro ao adicionar item", error);
-        });
-    }
-  
- 
-  const enviardespacho=()=>{
+    await api.post('/itensorcamento/pagamento', pagamento)
+      .then(res => {
+        if (res.status === 201) {
+          console.log(`Item adicionado com sucesso!`);
+          mostrarOrcamento();
+        } else {
+          console.error("Erro ao adicionar item", res.data);
+        }
+      })
+      .catch(error => {
+        console.error("Erro ao adicionar item", error);
+      });
+  }
+
+  const enviardespacho = () => {
     navigete(`/listarDespacho/${id}`)
   }
 
   return (
     <div className="dashboard-container">
-                            <Modal show={showPagar} onHide={fechaModalPagar}>
-                              <Modal.Header closeButton>
-                                  <Modal.Title>Pagamento</Modal.Title>
-                                      </Modal.Header>
-                                      <Modal.Body>
-                                          <div>
-                                          <div className='container_adicionar'>
-                                            <label> Tipo de Pagamento</label>
-                                          <select onChange={e => setTipo(e.target.value)}>
-                                          <option value="">Tipo de Pagamento</option>
-                                          <option value="Cartão Debito">Cartão Debito</option>
-                                          <option value="Cartão Crédito">Cartão Crédito</option>
-                                          <option value="A vista">À vista</option>
-                                          <option value="A prazo">À prazo</option>
-                                        </select>
-                                        </div>
-                                        <div className='container_adicionar'>
-                                        <label>Valor Recebido</label>
-                                        <input
-                                          type="number"
-                                          placeholder='Valor Recebido'
-                                          value={recebido}
-                                          
-                                        />
-                                        </div>
-                                        <div className='container_adicionar'>
-                                              <label>Troco</label>
-                                          <input
-                                            type="text"
-                                            placeholder='Troco'
-                                            value={formatarMoeda(troco)}
-                                            readOnly
-                                          />
-                                    
-                                    </div>
-                                        <div className='container_adicionar'>                                        <button class='btn btn-success' onClick={salvarPagamento}>Salvar Pagamento</button>
-                                        </div>
-                                          </div>
+      <Modal show={showPagar} onHide={fechaModalPagar}>
+        <Modal.Header closeButton>
+          <Modal.Title>Pagamento</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <div className='container_adicionar'>
+              {tipo}
+              <label> Tipo de Pagamento</label>
+              <select onChange={e => setTipo(e.target.value)}>
+                <option value="0">Tipo de Pagamento</option>
+                <option value="1">Cartão Debito</option>
+                <option value="2">Cartão Crédito</option>
+                <option value="3">À vista</option>
+                <option value="4">À prazo</option>
+              </select>
+            </div>
 
-                                      </Modal.Body>
-                            </Modal>
+            {tipo === "4" ? (
+              <div className='container-aprazo'>
+                <div>
+                <div className='container_adicionar'>
+                  <label>Qtd Parcelas</label>
+                  <input type="text" />
+                  </div>
+                  <div className='container_adicionar'>
+                  <label>Valor da Entrada</label>
+                  <input type="text" />
+                  </div>
+                  <div className='container_adicionar'>                 
+                  <label>1º Vencimento</label>
+                  <input type="date" />
+                  </div>
+                  <div className='container_adicionar'>
+                      <button class='btn btn-success' onClick={gerarParcelas}>Gerar Parcelas</button>
+                </div>
+                </div>
+                <div>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Ordem</th>
+                        <th>Orçamento</th>
+                        <th>Vlr Parcela</th>
+                        <th>Vencimento</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td> <td>2</td> <td>200</td> <td>26/07/2024</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            ) : (
+              <div className='container-avista'>
+                <div className='container_adicionar'>
+                  <label>Valor Recebido</label>
+                  <input
+                    type="text"
+                    placeholder='Valor Recebido'
+                    onChange={e => handleRecebidoChange(e.target.value)}
+                    value={recebido}
+                  />
+                </div>
+                <div className='container_adicionar'>
+                  <label>Troco</label>
+                  <input
+                    type="text"
+                    placeholder='Troco'
+                    value={formatarMoeda(troco)}
+                    readOnly
+                  />
+                </div>
+                <div className='container_adicionar'>
+              <button class='btn btn-success' onClick={salvarPagamento}>Salvar Pagamento</button>
+            </div>
+              </div>
+              
+            )}
 
-                      <Modal show={show} onHide={fechaModal}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Adicionar Produto</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <div>
-                                  <div className='container_adicionar'>
-                                  <label>Selecionar um Produto</label>
-                                  <select className='select-produto' value={id_produto} 
-                                   onChange={e => setId_produto(e.target.value)}>
-                                  <option value="">Selecione um produto</option>
-                               {produtos.map(produto => (
-                              <option key={produto.id} value={produto.id}>
-                            {produto.descricao}
-                          </option>
-                          
-                           ))}
-                            </select>
-                            </div>
-                            <div className='container_adicionar'>
-                         <label>Quantidade</label>
-                           <input 
-                            placeholder='Qtd em metros' 
-                            value={qtde}
-                            type="text"
-                            onChange={e => setQtde(e.target.value)}
-                          />
-                          </div>
-                          <div className='container_adicionar'>
-                        <label>Valor Unitário</label>
-                        <input 
-                          placeholder='Valor unitário' 
-                          type="text"
-                          value={valor_unitario}
-                          onChange={e => setValor_unitario(e.target.value) }
-                        />
-                        </div>
-                         <div className='container_adicionar'>
-                       <label>Total</label>
-                            <input
-                              placeholder='Total'
-                              type="text"
-                              value={tot}
-                              onChange={e =>setTot(e.target.value) }                       
-                      
-                      />  
-                      </div> 
-                      <div className='container_adicionar'>          
-                          
-                            <button type="button" onClick={salvarDados} class="btn btn-success">Salvar</button>
-                           
-                    </div>
+          </div>
+        </Modal.Body>
+      </Modal>
 
-
-                            </div>
-                            </Modal.Body>
-                    </Modal>
+      <Modal show={show} onHide={fechaModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Adicionar Produto</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <div className='container_adicionar'>
+              <label>Selecionar um Produto</label>
+              <select className='select-produto' value={id_produto}
+                onChange={e => setId_produto(e.target.value)}>
+                <option value="">Selecione um produto</option>
+                {produtos.map(produto => (
+                  <option key={produto.id} value={produto.id}>
+                    {produto.descricao}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className='container_adicionar'>
+              <label>Quantidade</label>
+              <input
+                placeholder='Qtd em metros'
+                value={qtde}
+                type="text"
+                onChange={e => setQtde(e.target.value)}
+              />
+            </div>
+            <div className='container_adicionar'>
+              <label>Valor Unitário</label>
+              <input
+                placeholder='Valor unitário'
+                type="text"
+                value={valor_unitario}
+                onChange={e => setValor_unitario(e.target.value)}
+              />
+            </div>
+            <div className='container_adicionar'>
+              <label>Total</label>
+              <input
+                placeholder='Total'
+                type="text"
+                value={tot}
+                onChange={e => setTot(e.target.value)}
+              />
+            </div>
+            <div className='container_adicionar'>
+              <button type="button" onClick={salvarDados} class="btn btn-success">Salvar</button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
       <Barrasuperior />
       <div className='dashboard-main'>
         <div className='principal'>
@@ -336,14 +373,9 @@ async  function mostrarOrcamento() {
             <p>Orçamento: {numeroorcamento}</p>
             <p>Total: {formatarMoeda(total)}</p>
             <p>Situação: {situacao}</p>
-            <button type="button" onClick={abrirModal}  class="btn btn-primary">Adicionar</button>
-            <button type="button" onClick={enviardespacho}   class="btn btn-secondary">Imprimir/Despachar</button>
+            <button type="button" onClick={abrirModal} class="btn btn-primary">Adicionar</button>
+            <button type="button" onClick={enviardespacho} class="btn btn-secondary">Imprimir/Despachar</button>
             <button type="button" onClick={abrirModalPagar} class="btn btn-info">Pagamento</button>
-            {
-                    situacao==="ABERTO"?
-                    
-                    <Link to={`/listarCaixa/${id}`} className='btn-fechar' style={{ marginRight: '10px' }}>Pagar</Link>:""
-                  }
           </div>
 
           <table>
@@ -358,31 +390,25 @@ async  function mostrarOrcamento() {
               </tr>
             </thead>
             <tbody>
- 
               {
-              
-         
-              orcamentos.map((orcamento, index) => (
-                <tr key={orcamento.id}>
-                  <td>{index + 1}</td>
-                  <td>{orcamento.descricao}</td>
-                  <td>{orcamento.quantidade}</td>
-                  <td>{formatarMoeda(orcamento.valor_unitario)}</td>
-                  <td>{formatarMoeda(orcamento.total)}</td>
-                  <td>
-                    <FiTrash
-                      size={18}
-                      color='red'
-                      onClick={() => confirmarExclusao(orcamento.id)}
-                      cursor="pointer"
-                    />
-                  </td>
-                </tr>
-              ))
-              
-             
+                orcamentos.map((orcamento, index) => (
+                  <tr key={orcamento.id}>
+                    <td>{index + 1}</td>
+                    <td>{orcamento.descricao}</td>
+                    <td>{orcamento.quantidade}</td>
+                    <td>{formatarMoeda(orcamento.valor_unitario)}</td>
+                    <td>{formatarMoeda(orcamento.total)}</td>
+                    <td>
+                      <FiTrash
+                        size={18}
+                        color='red'
+                        onClick={() => confirmarExclusao(orcamento.id)}
+                        cursor="pointer"
+                      />
+                    </td>
+                  </tr>
+                ))
               }
-          
             </tbody>
           </table>
         </div>
